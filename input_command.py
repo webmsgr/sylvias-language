241dcb87b77718968b5fb34624eb1d12fa2e073d
Wackery Smackery <zapcontact000@gmail.com>
2018-12-17 15:14:54 -0800

    question my sanity

--- input_command.py
+++ input_command.py
@@ -3,4 +3,4 @@ def cinput(args):
     arg = args[0]
     return input(arg)
 def makeCommands(func):
-    func("input",cinput,1,False)
+    func("input",cinput,1,False) # why do i have only this function in this file? i dont know!
